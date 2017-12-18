87ad5a231779ba48a0e03e5ff7205e31b95317d8
libin <libin.xtu@foxmail.com>
2017-12-18 19:24:03 +0800

    更新：地址获取和正文抓取功能分开；对地址和浮点筛选后存入文件

--- filter_annual_report_url.py
+++ filter_annual_report_url.py
@@ -9,8 +9,8 @@
 
 
 import configparser as ConfigParser
+import csv
 import logging
-import random
 import re
 import time
 import urllib.request
@@ -20,11 +20,11 @@ from bs4 import BeautifulSoup
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
 local_reports_collect_url = config['url']['collect_url']
-root_file_path = config['file_path']['root_directory']
+file_path = config['file_path']['url_data_file']
 
 
 def get_standard_html(url):
-    """获取汇总合集页面的标准 HTML 文本
+    """获取网页的标准 HTML 文本
     :return: 
     """
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
@@ -34,7 +34,7 @@ def get_standard_html(url):
     try:
         html = urllib.request.urlopen(req, timeout=60).read()
         standard_html = BeautifulSoup(html, 'html5lib')
-    except Exception as e:
+    except Exception as e:  # 如果抓取失败则退出，并令 standard_html 为 None
         logging.info(url + "抓取超时......")
         standard_html = None
         pass
@@ -42,7 +42,7 @@ def get_standard_html(url):
 
 
 def filter_report_collect_url(standard_html):
-    """过滤得到汇总页面中历年地方政府报告合集页面的 URL
+    """从 standard_html 中过滤得到历年地方政府报告合集页面的 URL
     :return: url_dict，
     [{'year': "2017年汇编", 'url': 'http://district.ce.cn/zg/201702/26/t20170226_20528713.shtml'}]
     """
@@ -59,7 +59,7 @@ def filter_report_collect_url(standard_html):
 
 
 def filter_local_gov_report_url(url):
-    """地方政府报告合集页面的省市发布的 URL
+    """地方政府报告合集页面的省市报告的 URL
     :param url: 
     :return: 
     [{'report_title': '北京市政府工作报告 (2017年1月14日 蔡奇)', 
@@ -68,12 +68,13 @@ def filter_local_gov_report_url(url):
     local_gov_report_url_dict = []
     standard_html = get_standard_html(url)
     raw_html_text = standard_html.find_all('div', class_='TRS_Editor')
+    """个别网页的链接在 div 标签的 content 类中"""
     if len(raw_html_text) > 0:
         html_text = raw_html_text[0].find_all('a')
         for text in html_text:
             temp_dict = {
-                'report_title': text.get_text(),
-                'report_url': text.get('href')
+                'report_title': (text.get_text()).replace('\xa0', '').replace('资料:', ''),  # 1. 替换 \xa0，防止写入 csv 错误
+                'report_url': text.get('href')                                              # 2. 替换“资料:”，文件名中有冒号无法写入
             }
             local_gov_report_url_dict.append(temp_dict)
     else:
@@ -81,7 +82,7 @@ def filter_local_gov_report_url(url):
         html_text = raw_html_text[0].find_all('a')
         for text in html_text:
             temp_dict = {
-                'report_title': text.get_text(),
+                'report_title': (text.get_text()).replace('\xa0', '').replace('资料:', ''),
                 'report_url': text.get('href')
             }
             local_gov_report_url_dict.append(temp_dict)
@@ -96,97 +97,37 @@ def get_annual_report_url_list(url_dict):
     """
     annual_report_url_list = []       # 年度省（直辖市）报告 URL 列表：2003-2017 年所有省市报告的地址
     for item in url_dict:
-        report_url = item.get('url')  # report_url 是某年报告汇编的地址，如“2017年汇编”
+        report_url = item.get('url')  # report_url 是某年报告汇编合集的地址
         local_gov_report_url = filter_local_gov_report_url(report_url)
         annual_report_url_list.append(local_gov_report_url)
         print(report_url)
-        time.sleep(2)
+        time.sleep(2)                 # 为了防止 ip 被封，每抓完一年延迟两秒
     return annual_report_url_list
 
 
-
-
-
-def get_report_text(url):
-    """从 URL 地址获取网页正文部分
-    :param local_report_url: 
-    :return: 
-    """
-    standard_html = get_standard_html(url)
-    if standard_html:
-        raw_html_text = standard_html.find_all('p')
-        text = []
-        for item in raw_html_text:
-            text.append(item.get_text())
-        return text
-
-
-def get_page_sum(url):
-    """
-    获取报告总页数：共（n）页
-    :param url: 
-    :return: 
-    """
-    standard_html = get_standard_html(url)
-    tmp_html = standard_html.find_all('script', language='JavaScript')
-    tmp_str = re.findall(r'[^createPageHTML(].*[$,]', str(tmp_html))
-    page_sum = tmp_str[1][16]
-    return int(page_sum)
-
-
-def generate_page_urls(url, page_sum):
-    """
-    当报告有多个页面时，生成多页面的 URL
-    :param page_num_sum: 
-    :return: 
-    """
-    i = 1
-    url_list = [url]
-    while i < page_sum:
-        page_url = ''.join([url[:-6], '_', str(i), '.shtml'])
-        url_list.append(page_url)
-        i += 1
-    return url_list
-
-
-def crawl_province_report(report_title, province_report_url):
-    """抓取报告全文并写入 txt 
-    
-    """
-    global file_path
-    page_sum = get_page_sum(province_report_url)
-    url_list = generate_page_urls(province_report_url, page_sum)
-    text = []
-    for page_url in url_list:
-        tmp_text = get_report_text(page_url)
-        text.append(tmp_text)
-        file_path = root_file_path + str(report_title) + '.txt'
-        print(page_url)
-        time.sleep(random.randint(0, 9))
-    flatten_text =  [item for sublist in text for item in sublist]
-    report_text_handler = open(file_path, "ab+")
-    for item in flatten_text:
-        report_text_handler.write((item + '\r\n').encode('UTF-8'))
-        print('正在写入文件...')
-
-
-def get_all_report(annual_report_url_list):
-    """
-    从省市报告地址中提取出网页正文，并写入 TXT 文件
-    :param annual_report_url_list: 
-    :return: 
-    """
-    for item in annual_report_url_list:
-        for sub_item in item:
-            report_title = sub_item.get('report_title')
-            province_report_url = sub_item.get('report_url')
-            if re.match(r'^https?:/{2}\w.+$', province_report_url) and \
-                    re.match(r'[^?!(\d) | ^?!(（)]', report_title) and len(report_title) > 6:
-                crawl_province_report(report_title, province_report_url)
-            time.sleep(2)
+def write_data_to_file(url_list):
+    with open(file_path, 'w', newline='') as f:
+        fieldnames = ['report_title', 'report_url']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for item in url_list:
+            for sub_item in item:
+                report_title = sub_item.get('report_title')
+                province_report_url = sub_item.get('report_url')
+                """
+                1. 判断 URL 地址是否合法
+                2. 判断标题是否数字或者以中文括号‘（’开头，如果是，则不是报告地址
+                3. 标题长度至少大于 6：“政府工作报告”
+                """
+                if re.match(r'^https?:/{2}\w.+$', province_report_url) and \
+                    re.match(r'[^?!(\d) | ^?!(（)]', report_title) and \
+                    len(report_title) > 6:
+                    writer.writerow(sub_item)
+    f.close()
 
 
 if __name__ == '__main__':
-    a = get_standard_html(local_reports_collect_url)
-    b = filter_report_collect_url(a)
-    d = get_annual_report_url_list(b)
+    standard_html = get_standard_html(local_reports_collect_url)
+    url_dict = filter_report_collect_url(standard_html)
+    annual_report_url_list = get_annual_report_url_list(url_dict)
+    write_data_to_file(annual_report_url_list)
